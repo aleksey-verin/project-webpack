7b9ec0679535a39e294d104cf3f9bb263d5fc30b
Aleksey Verin <verevaa@yandex.ru>
2023-08-07 12:56:31 +0300

    feat: added MiniCssExtractPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,5 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import path from 'path'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
@@ -9,5 +10,9 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthans:8].css',
+      chunkFilename: 'css/[name].[contenthans:8].css',
+    }),
   ]
 }
